28e78da55937e0a76272510e604a3ba7bd03d789
willxvi <53283150+willxvi@users.noreply.github.com>
2019-07-25 10:42:48 -0300

    Create events.js

---
+++ arcard/events.js
@@ -0,0 +1,14 @@
+AFRAME.registerComponent('markerhandler', {
+
+    init: function() {
+        const animatedMarker = document.querySelector("#animated-marker");
+        const aEntity = document.querySelector("#animated-model");
+
+        // every click, we make our model grow in size :)
+        animatedMarker.addEventListener('click', function(ev, target){
+            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+            if (aEntity && intersectedElement === aEntity) {
+                window.open("https://www.google.com");
+            }
+        });
+}});
