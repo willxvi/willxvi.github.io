d243f93caf969768d1fa901059a34395a1d0e35e
willxvi <53283150+willxvi@users.noreply.github.com>
2019-07-25 13:41:36 -0300

    Update events2.js

--- arcard/events2.js
+++ arcard/events2.js
@@ -3,12 +3,16 @@ AFRAME.registerComponent('markerhandler', {
     init: function() {
         const animatedMarker = document.querySelector("#animated-marker");
         const aEntity = document.querySelector("#animated-model");
+        const aEntity2 = document.querySelector("#animated-model2");
 
         // every click, we make our model grow in size :)
         animatedMarker.addEventListener('click', function(ev, target){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
             if (aEntity && intersectedElement === aEntity) {
-                window.open("https://www.google.com");
+                console.log("Yellow Box");
+            }
+            if (aEntity2 && intersectedElement === aEntity2) {
+                console.log("Blue Box");
             }
         });
 }});
