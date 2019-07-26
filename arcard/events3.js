0a8b6db6e77f14f93aa90edad686bc140eb5304d
willxvi <53283150+willxvi@users.noreply.github.com>
2019-07-26 12:47:42 -0300

    Update events3.js

--- arcard/events3.js
+++ arcard/events3.js
@@ -9,12 +9,12 @@ AFRAME.registerComponent('markerhandler', {
         animatedMarker.addEventListener('mousedown', function(ev, target){
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
             if (aEntity && intersectedElement === aEntity) {
-				aEntity.setAttribute('animation', {property: 'rotation'; dur: 3000; to: '0 360 0'; loop: true})		
+				aEntity.setAttribute('animation', {property: 'rotation'; dur: 3000; to: '0 360 0'; loop: true});		
 				alert("Yellow");
             }
             if (aEntity2 && intersectedElement === aEntity2) {
 				const position = aEntity2.getAttribute('position');
-				aEntity2.setAttribute('animation', {property: 'rotation'; dur: 3000; to: '0 360 0'; loop: true})				
+				aEntity2.setAttribute('animation', {property: 'rotation'; dur: 3000; to: '0 360 0'; loop: true});				
 				alert("Blue");
 				
             }
