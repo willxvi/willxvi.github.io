8c596145ffa8aed7b9527af6a25104bf671a1750
willxvi <53283150+willxvi@users.noreply.github.com>
2019-07-26 13:45:42 -0300

    Update events3.js

--- arcard/events3.js
+++ arcard/events3.js
@@ -10,13 +10,13 @@ AFRAME.registerComponent('markerhandler', {
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
             if (aEntity && intersectedElement === aEntity) {
 				aEntity.setAttribute('animation', {property: 'rotation', dur: 3000, to: '0 360 0', loop: true});
-				aEntity.setAttribute('animation', {attribute: 'position', dur: 1000, to: '0 0.5 0', direction="alternate"});					
+				aEntity.setAttribute('animation', {property: 'position', dur: 1000, to: '0 0.5 0', direction="alternate"});					
 				alert("Yellow");
             }
             if (aEntity2 && intersectedElement === aEntity2) {
 				const position = aEntity2.getAttribute('position');
 				aEntity2.setAttribute('animation', {property: 'rotation', dur: 3000, to: '0 360 0', loop: true});
-				aEntity2.setAttribute('animation', {attribute: 'position', dur: 1000, to: '0 0.5 0', direction="alternate"});					
+				aEntity2.setAttribute('animation', {property: 'position', dur: 1000, to: '0 0.5 0', direction="alternate"});					
 				alert("Blue");
 				
             }
