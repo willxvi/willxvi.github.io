1c489196adbad380834ed9a9bd090c12bfa1985f
willxvi <53283150+willxvi@users.noreply.github.com>
2019-07-27 18:24:23 -0300

    Update eventsGltf.js

--- arcard/eventsGltf.js
+++ arcard/eventsGltf.js
@@ -1,13 +1,11 @@
 AFRAME.registerComponent('markerhandler', {
 
-	
     init: function() {
 		
 		const aEntity = document.querySelector("#animated-model");
 		const animatedMarker = document.querySelector("#animated-marker");
 		
 		animatedMarker.addEventListener('markerFound', function() {
-			alert("marker");
-			aEntity.setAttribute('animation-mixer', {loop: 'repeat'});	
+			aEntity.setAttribute('animation-mixer', {loop: 'once'});	
 		});
 }});
