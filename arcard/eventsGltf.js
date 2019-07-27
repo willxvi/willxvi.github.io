467e2c64fac698439cc0938e0252a09f582bb09a
willxvi <53283150+willxvi@users.noreply.github.com>
2019-07-27 18:15:35 -0300

    request

--- arcard/eventsGltf.js
+++ arcard/eventsGltf.js
@@ -3,13 +3,11 @@ AFRAME.registerComponent('markerhandler', {
 	
     init: function() {
 		
-		alert("teste");
-		
 		const aEntity = document.querySelector("#animated-model");
 		const animatedMarker = document.querySelector("#animated-marker");
 		
-		if(animatedMarker.object3D.visible == true){
+		animatedMarker.addEventListener('markerFound', function() {
 			alert("marker");
 			aEntity.setAttribute('animation-mixer', {loop: 'repeat'});	
-		};
+		});
 }});
