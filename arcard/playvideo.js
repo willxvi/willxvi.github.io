1f80d9ac7a5d7ae46bc0ea94b5dd21be03048fa6
willxvi <53283150+willxvi@users.noreply.github.com>
2019-08-31 16:12:58 -0300

    video

--- arcard/playvideo.js
+++ arcard/playvideo.js
@@ -2,7 +2,7 @@ AFRAME.registerComponent('markerhandler', {
 
     init: function() {
 		
-		const aEntity = document.querySelector("#video1");
+		const aEntity = document.querySelector("#vid");
 		const animatedMarker = document.querySelector("#animated-marker");
 		
 		animatedMarker.addEventListener('markerFound', function() {
